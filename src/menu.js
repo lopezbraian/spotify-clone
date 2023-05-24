033478152575fe586f1985fd67ebbfa94d84779a
Jole <yole126@gmail.com>
2023-05-24 18:33:41 -0300

    first commit

---
+++ src/menu.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Menu = () => {
+    return <nav>
+        <ul>
+            <li>Premium</li>
+            <li>Support</li>
+            <li>Download</li>
+            <li> | </li>
+            <li>Sign up</li>
+            <li>Log in</li>
+        </ul>
+    </nav>
+};
+
+export default Menu;
